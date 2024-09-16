466ad1cba2dbe85fa9cc92b118ba33aadd932b25
kammmkammm <135635730+kammmkammm@users.noreply.github.com>
2024-09-17 04:17:33 +1400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: Yes, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
+    "unbl0ck3dgam3s.vercel.app": "",
     localhost: "",
   },
   users: {
