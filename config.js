070402e617dd87a8c0c202322869d0c6edb38126
kammmkammm <135635730+kammmkammm@users.noreply.github.com>
2024-09-26 05:06:17 +1400

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ const config = {
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    Kam: "kamisbetter",
-    Everett: "everett",
-        Bobby: "Thebigbobbyai",
+
     
   },
 };
